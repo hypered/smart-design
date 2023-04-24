0da498232f9d5ebce3bf5ce027e3a27e275deed8
Vo Minh Thu <thu@hypered.io>
2023-04-25 01:04:59 +0200

    Remove unused pageTree / prototypeNav.

--- core/js/index.js
+++ core/js/index.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
 
-import './prototype-nav';
 import './styleguide-code-samples';
 import './styleguide-typography';
 import './styleguide-search';
@@ -8,6 +7,5 @@ import rememberScroll from './styleguide-remember-scroll';
 
 window.$ = $;
 
-rememberScroll('#__prototype-nav');
 rememberScroll('.br-styleguide-content');
 rememberScroll('.br-styleguide-navigation-holder');
